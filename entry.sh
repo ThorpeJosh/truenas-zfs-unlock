75bf9df4b607bb2feadff4b792d847ae8d68d450
thorpejosh <thorpe.josh@outlook.com>
2023-10-27 07:11:51 +0800

    cicd: Dockerfile and entry script

---
+++ entry.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ -n "$CRONTAB" ]; then
+  echo "$CRONTAB" > /crontab.txt
+else
+  echo "Error: Missing 'CRONTAB' environment variable"
+  exit 1
+fi
+
+# Test crontab
+echo "Testing crontab.txt"
+supercronic -test -debug /crontab.txt || exit 1
+
+# Start supercronic
+supercronic -passthrough-logs /crontab.txt
